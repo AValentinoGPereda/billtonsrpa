3beb2aeb4c38b36486c580b603281d8ad42f8294
AValentinoGPereda <angel_garcia3@usmp.pe>
2025-05-11 19:01:48 -0500

    💥 Limpieza total: borrar todo el contenido

--- proyecto-billtons-p2/src/controllers/UserController.js
+++
@@ -1,7 +0,0 @@
-// src/controllers/UserController.js
-import { fetchAllUsers } from '@models/UserModel'
-
-export function getAllUsers() {
-  // Podrías añadir aquí lógica de negocio
-  return fetchAllUsers()
-}
